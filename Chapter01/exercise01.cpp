15fb24846a154406c71c3879d5e64d9b3d1235f9
Nian Kim <niankim@gmail.com>
2026-01-05 18:26:05 +0100

    updating my current files

---
+++ Chapter01/exercise01.cpp
@@ -0,0 +1,10 @@
+//This program is testing the possibilities of the character '\n' and std::endl
+#include <iostream>
+
+int main(){
+    std::cout << "This is a line of text!" << std::endl;
+    std::cout << "This is too!\n";
+    std::cout << "Can you spot the difference here?\n" << std::endl;
+    std::cout << "\nThere's much" << '\n' << "to be played around with~" << std::endl;
+    return 0;
+}
