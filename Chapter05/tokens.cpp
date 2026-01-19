ffe4ed25abca854adbb657eef740823df56917f3
Nian Kim <niankim@gmail.com>
2026-01-19 20:58:52 +0100

    Created first constructor

--- Chapter05/tokens.cpp
+++ Chapter05/tokens.cpp
@@ -2,14 +2,16 @@
 //a very simple user-defined type (pair)
 class Token {
     public:
-    char kind;
-    double value;
+        char kind;
+        double value;
+        //initialize construct Tokens
+        Token(char k) :kind{k}, value {0.0}{} //construct from one value
+        Token(char k, double v) :kind{k}, value {v}{} //construct from two values
+        
 };
 
 int main(){
-    Token t;
-    t.kind = '+'; //t represents a +
-    Token t2;
-    t2.kind = '8'; //digit '8' as the kind for numbers
-    t2.value = 3.14;
+Token t1 {'+'};                      // initialize t1 so that t1.kind = ’+’
+Token t2 {'8',11.5};              // initialize t2 so that t2.kind = ’8’ and t2.value = 11.5
+Token t3;                             // error: initializer missing for t3
 }
