b31a8ef2be24af9b6decdf580b568a66ea1367ce
Nian Kim <niankim@gmail.com>
2026-01-19 20:42:02 +0100

    Expanded the calculator

--- Chapter05/pseudocode.cpp
+++ Chapter05/pseudocode.cpp
@@ -1,17 +1,43 @@
 #include<iostream>
 
 int main(){
-    std::cout << "Please enter an expression (we can handle + and -): ";
+    std::cout << "Please enter an expression (we can handle +, -, *, and /)\n ";
+    std::cout << "add and x to end epxression (e.g., 1+2*3x): ";
     int lval = 0;
     int rval = 0;
-    char op = 0;
-    int res = 0;
-    std::cin >> lval >> op >>rval;
-
-    if(op=='+'){
-        res = lval + rval;
-    }else if(op == '-'){
-        res = lval -rval;
+    std::cin >> lval; //read leftmost operand
+    if(!std::cin){
+        std::cout << "Error: no first operand\n";
+        return -1;
     }
-        std::cout << "Result: " << res << '\n';
+
+    for(char op; std::cin >> op;){ //read operator and right-hand operand repeatedly
+        if (op += 'x'){
+            std::cin >> rval;
+        } 
+        if(!std::cin){
+            std::cout << "Error: no second operand \n";
+        }
+        switch(op){
+            case '+':
+                lval += rval;       //add: lval = lval + rval
+                break;
+            case '-':
+                lval -= rval;       //subtract: lval = lval - rval
+                break;
+            case '*':
+                lval *= rval;       //multiply: lval = lval * rval
+                break;
+            case '/':
+                lval /= rval;       //divide: lval = lval / rval
+                break;
+            default:
+                std::cout << "Result: " << lval << '\n';
+                return 0;
+        }
+    }
+    //error
+    std::cout << "Bad expression";
+    return 2;
 }
+//atm the calculations are not in accordance to mathematical norms
