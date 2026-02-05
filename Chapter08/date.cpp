c10d9c5012f2829da1119ba321144f771372dbef
Nian Kim <niankim@gmail.com>
2026-02-05 22:22:49 +0000

    Completed Try_this02 of Chapter 08

--- Chapter08/date.cpp
+++ Chapter08/date.cpp
@@ -13,38 +13,121 @@ Test each version with at least one invalid date (e.g., 2004, 13, -5).
 */
 
 #include<iostream>
+#include<vector>
 using namespace std;
+
+//types first
+enum class Month {
+         jan=1, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec
+};
+
+int to_int(Month m)
+{
+         return static_cast<int>(m);
+}
+Month operator++(Month& m){
+    m = (m==Month::dec) ? Month::jan : Month{to_int(m)+1}; //arithmetic if
+    return m;
+}
+//see line 42
+ostream& operator<<(ostream& os, Month m){
+    static const vector<string> month_tbl = {
+        "Not_A_Month", "Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    };
+    return os << month_tbl[to_int(m)];
+}
+
+//helper functions
 
 class Date{
     public:
-        //Date(int y, int m, int d); //check for valid date and initialize 
+        class Invalid{}; //exception
+        //check for valid date and initialize 
         Date(int yy, int mm, int dd) //constructor
             :y{yy}, m{mm}, d{dd}            //note: member initializers
-            {}
-        int month() {return Date::m;}
-        int day() {return Date::d;}
-        int year() {return Date::y;}
+            {
+                if(!is_valid()){
+                    throw Invalid{};
+                }
+            }
+        Month month() const{return Date::m;} //getting the value shouldn't change it
+        int day() const{return Date::d;}
+        int year() const{return Date::y;}
+
+        //prevent invalid dates:
+        bool is_valid();
+        void add_day(int n);
+
     private:
-        int y, m, d; //year, month, day
+        int y, d; //year, day
+        Month m; //month
 };
 
 //help from Stroustrup
+//passing into ostream& os (console window) whenever you see '<< Date_obj'
 ostream& operator<<(ostream& os, Date d)
     {
             return os << d.year() << '/' << d.month() << '/' << d.day();
     }
 
-
+/*
 void init_day(Date& dd, int y, int m, int d){
     //check that (y,m,d) is a valid date. If it is, use it to init dd
 }
+*/
 
-void add_day(Date& dd, int n){
-    //increase dd by n days
+//ignoring leap years and different month lengths
+bool Date::is_valid(){
+    if(d<1||d>31)return false;
+    return true;}
+
+void Date::add_day(int n){
+    d+=n;
+    while (d>31){
+    ++m;
+    d-=31;
+    if(m==Month::jan)++y;
+    }
 }
-int main(){
-    Date today{2025, 2, 4};
 
-    cout << "Today is: " << today << '\n';
+void error(string s){
+    throw runtime_error{s};
+}
+
+void f(int x, int y){
+    try{
+        Date dxy {2025,x,y};
+        cout << dxy << '\n';
+        dxy.add_day(2);
+    }catch(Date::Invalid){
+        error("f(): invalid date");
+    }
+}
+
+
+int main(){
+    try{
+        Date today{2025, 2, 4};
+        cout << "Today is: " << today << '\n'; 
+
+        Date tomorrow = today;
+        tomorrow.add_day(1);
+        cout << "Tomorrow is: " << tomorrow << '\n';
+
+        cout << "\nTesting Month Wrap: \n";   
+        Month m = Month::dec;
+        cout << "Current: " << m << '\n';
+        ++m;
+        cout << "Next:    " << m << " (Should be Jan)\n";
+    }
+    catch(Date::Invalid){
+        cerr << "Error: INvalid Date!\n";
+        return 1;
+    }
+    catch(exception& e){
+        cerr << "Error: " << e.what() << '\n';
+        return 2;
+    }
     return 0;
 }
