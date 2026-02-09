656e5b5715735509e1f638495797b0ae60a2157f
Nian Kim <niankim@gmail.com>
2026-02-09 11:20:13 +0000

    Finished the Chapter 09 review.txt

--- Chapter09/decimal_to_hex.cpp
+++ Chapter09/decimal_to_hex.cpp
@@ -44,3 +44,25 @@ int get_int_range(int low, int high)
                 cout << "Sorry " << n << " is not in the [" << low << ":" << high << "] range; please try again\n";
             }
 }
+
+int get_int_range_greeting(int low, int high, const string& greeting, const string& sorry)
+{
+        cout << greeting << ":[" << low << ":" << high << "]\n";
+        while (true) {
+            int n = get_int();                                    // we got an integer; now check it
+            if (low<=n && n<=high)
+                        return n;
+                cout << sorry << ": [" << low << ":" << high << "]\n";
+            }
+}
+
+void end_of_loop(istream& ist, char term, const string& message)
+{
+         if (ist.fail()) {                                // use term as terminator and/or separator
+                  ist.clear();
+                  char ch = 0;
+                  if (ist>>ch && ch==term)
+                           return;                       // all is fine
+                  error(message);
+         }
+}
