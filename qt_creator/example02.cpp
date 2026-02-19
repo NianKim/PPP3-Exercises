f7ba4332a7e70644bab1180f12c91fc85bbd80fe
Nian Kim <niankim@gmail.com>
2026-02-19 22:05:42 +0100

    Finished chapter 10 drill

--- qt_creator/example02.cpp
+++ qt_creator/example02.cpp
@@ -34,4 +34,85 @@ int main()
     win.attach(sine);
     win.set_label("Sine");
     win.wait_for_button();
+
+    sine.set_color(Color::blue);             // we changed our mind about sine’s color
+
+    Polygon poly;                                     // a polygon; a Polygon is a kind of Shape
+    poly.add(Point{300,200});                  // three points make a triangle
+    poly.add(Point{350,100});
+    poly.add(Point{400,200});
+    poly.set_color(Color::red);
+    win.attach(poly);
+    win.set_label("Triangle");
+    win.wait_for_button();
+
+    Rectangle r {Point{200,200}, 100, 50};              // top left corner, width, height
+
+    win.attach(r);
+    win.set_label("Rectangle");
+    win.wait_for_button();
+
+    Closed_polyline poly_rect;
+    poly_rect.add(Point{100,50});
+    poly_rect.add(Point{200,50});
+    poly_rect.add(Point{200,100});
+    poly_rect.add(Point{100,100});
+
+
+    win.set_label("Polyline");
+    win.attach(poly_rect);
+    win.wait_for_button();
+    poly_rect.add(Point{50,75});
+    win.set_label("Polyline 2");
+    win.wait_for_button();
+
+    r.set_fill_color(Color::yellow);         // color the inside of the rectangle
+    poly.set_style(Line_style(Line_style::dash,4));
+    poly_rect.set_style(Line_style(Line_style::dash,2));
+    poly_rect.set_fill_color(Color::green);
+    win.set_label("Fill");
+    win.wait_for_button();
+
+    Text t {Point{150,150}, "Hello, graphical world!"};
+    win.attach(t);
+    win.set_label("Text");
+    win.wait_for_button();
+
+    t.set_font(Font::times_bold);
+    t.set_font_size(20);
+    win.set_label("Bold text");
+    win.wait_for_button();
+
+    Image copter {Point{100,50},"mars_copter.jpg"};
+    win.attach(copter);
+    win.set_label("Mars copter");
+    win.wait_for_button();
+
+    copter.move(100,250);
+    win.set_label("Move");
+    win.wait_for_button();
+
+    Circle c {Point{100,200},50};
+
+    Ellipse e {Point{100,200}, 75,25};
+    e.set_color(Color::dark_red);
+
+    Mark m {Point{100,200},'x'};
+    m.set_color(Color::red);
+    ostringstream oss;
+    oss << "screen size: " << x_max() << "*" << y_max()
+        << "; window size: " << win.x_max() << "*" << win.y_max();
+    Text sizes {Point{100,20},oss.str()};
+
+    Image scan{ Point{275,225},"scandinavia.jfif" };
+    scan.scale(150,200);
+
+    win.attach(c);
+    win.attach(m);
+    win.attach(e);
+
+    win.attach(sizes);
+    win.attach(scan);
+    win.set_label("Final!");
+    win.wait_for_button();
 }
