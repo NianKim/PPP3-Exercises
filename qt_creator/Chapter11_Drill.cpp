264294c38821f6047c682c4688d88087c5702b89
Nian Kim <niankim@gmail.com>
2026-02-23 20:43:19 +0100

    finished chapter 11 drill

--- qt_creator/Chapter11_Drill.cpp
+++ qt_creator/Chapter11_Drill.cpp
@@ -12,7 +12,7 @@ int main()
 
     Simple_window win {x,800,1000,"grid"};
 
-    int x_size = win.x_max();                                     // get the size of our window
+    const int x_size = win.x_max();
     int y_size = win.y_max();
     int x_grid = 100;
     int y_grid = 100;
@@ -23,24 +23,45 @@ int main()
     for (int y = y_grid; y<y_size; y+=y_grid)
         grid.add(Point{0,y},Point{x_size,y});
 
-    grid.set_style(Line_style::dot);
+    grid.set_style(Line_style{Line_style::dot, 3});
     grid.set_color(Color::dark_green);
+
 
     win.attach(grid);
     win.wait_for_button();    // display!
 
+    Vector_ref<Rectangle> vr;
 
+    for (int i = 0; i < x_size; i+=100){
+            vr.push_back(make_unique<Rectangle>(Point{i, i}, 100, 100));
+            vr[vr.size() - 1].set_fill_color(Color::red);
+            win.attach(vr[vr.size() - 1]);
+            win.wait_for_button();
+    }
+
+
+
+    Vector_ref<Rectangle> image_200;
+    int offsets[2][2] = { {0, 200}, {200, 0} };
+
+    for(int j = 0; j < 2; ++j){
+        for (int i = 0; i < x_size; i+=200){
+            int current_x = i + offsets[j][0];
+            int current_y = i + offsets[j][1];
+
+            image_200.push_back(make_unique<Rectangle>(Point{current_x, current_y}, 200, 200));
+            image_200[image_200.size() - 1].set_fill_color(Color::blue);
+            win.attach(image_200[image_200.size() - 1]);
+
+            win.wait_for_button();
+        }
+    }
+    for (int j = 0; j < y_size; j+=100){
+        for (int i = 0; i < x_size; i+=100){
+            Rectangle rect_ii {Point{i,j},100,100};
+            rect_ii.set_fill_color(Color::white);
+            win.attach(rect_ii);
+            win.wait_for_button();
+        }
+    }
 }
-/*
-[1] Make an 800-by-1000 Simple_window.
-
-[2] Put an 8-by-8 grid on the leftmost 800-by-800 part of that window (so that each square is 100 by 100).
-
-[3] Make the eight squares on the diagonal starting from the top left corner red (use Rectangle).
-
-[4] Find a 200-by-200-pixel image (JPEG or GIF) and place three copies of it on the grid (each image covering four squares
- If you can’t find an image that is exactly 200 by 200, use set_mask() to pick a 200-by-200 section of a larger image. Don’t obscure the red squares.
-
-[5] Add a 100-by-100 image. Have it move around from square to square when you click the “Next” button. Just put wait_for_button() in a
-loop with some code that picks a new square for your image.
-*/
