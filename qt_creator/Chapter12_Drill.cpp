0c16781f125d5d7ee14c28c4004a17315ffe6906
Nian Kim <niankim@gmail.com>
2026-03-01 16:17:34 +0100

    Step 5 and 6 of Drill

--- qt_creator/Chapter12_Drill.cpp
+++ qt_creator/Chapter12_Drill.cpp
@@ -6,24 +6,30 @@ struct B1 {
 public:
     virtual void vf() const { cout << "B1::vf "; }
     void f() const { cout << "B1::f "; }             // not virtual
+    virtual void pvf() const =0;                     // pure virtual function
 };
 
-struct D1 : B1 {
+struct D1 : B1 {                                    //inherits pvf()
     void vf() const { cout << "D1::vf "; }           // overrides B1::vf
-    //void f() const { cout << "D1::f "; }
+    void f() const { cout << "D1::f "; }
+};
+
+struct D2 : D1{
+    void pvf() const override { cout << "D2::pvf "; }
+};
+
+struct B2 {
+    virtual void pvf() const =0;
 };
 
 int main()
 {
-    B1 b1;
-    b1.vf();
-    b1.f();
+    D2 d2;
+    d2.f();
+    d2.vf();
+    d2.pvf();
 
-    D1 d1;
-    d1.vf();
-    d1.f();
-
-    B1& b1_ref = d1;
+    B1& b1_ref = d2;
     b1_ref.vf();        //polymorphism
     b1_ref.f();         //not virtual in base class
 }
