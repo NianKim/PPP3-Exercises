5eb3a8ec32aa090008f235d940ea8b52b75f240d
Nian Kim <niankim@gmail.com>
2026-03-03 20:00:02 +0100

    Implemented a taylor series approximation of exp(x). This implementation starts to fail after n == 15 and crashes at n == 50. This may be due to the constraints of the double-type used to represent the function.

--- qt_creator/Chapter13.cpp
+++ qt_creator/Chapter13.cpp
@@ -8,6 +8,25 @@ double dsin(double d) {return std::sin(d);}
 double slope(double x) { return 0.5*x; }           // the slope is 0.5
 double square(double x) { return x*x; }
 double one(double) { return 1; }
+double dcos(double d) { return cos(d); }          // dcos() chooses cos(double)
+double sloping_cos(double x) { return cos(x)+slope(x); }
+int fac(int n)               // factorial(n); n!
+{
+    int r = 1;
+    while (n>1) {
+        r*=n;
+        --n;
+    }
+    return r;
+}
+double term(double x, int n) { return pow(x,n)/fac(n); }    // nth term of series
+double exp_n(double x, int n)             // sum of n terms for x
+{
+    double sum = 0;
+    for (int i=0; i<n; ++i)
+        sum+=term(x,i);
+    return sum;
+}
 
 int main()
 {
@@ -31,6 +50,8 @@ int main()
 
    Simple_window win {Point{100,100},xmax,ymax,"Three functions"};
 
+   //plots
+   /*
    Function s {one,r_min,r_max,orig,n_points,x_scale,y_scale};
    Function s2 {slope,r_min,r_max,orig,n_points,x_scale,y_scale};
    Function s3 {square,r_min,r_max,orig,n_points,x_scale,y_scale};
@@ -39,4 +60,68 @@ int main()
    win.attach(s2);
    win.attach(s3);
    win.wait_for_button();
+    */
+
+   /*
+   //labels
+   Text ts {Point{100,y_orig-40},"one"};
+   Text ts2 {Point{100,y_orig+y_orig/2-20},"0.5*x"};
+   Text ts3 {Point{x_orig-100,20},"x*x"};
+   win.set_label("Function graphing: label functions");
+   win.attach(ts);
+   win.attach(ts2);
+   win.attach(ts3);
+   win.wait_for_button();
+   */
+
+   //axis
+   constexpr int xlength = xmax-40; // make the axis a bit smaller than the window
+   constexpr int ylength = ymax-40;
+
+
+   Axis x {Axis::x,Point{20,y_orig}, xlength, xlength/x_scale, "one notch == 1"};
+   Axis y {Axis::y,Point{x_orig, ylength+20}, ylength, ylength/y_scale, "one notch == 1"};
+   x.set_color(Color::white);
+   y.set_color(Color::white);
+   win.attach(x);
+   win.attach(y);
+   win.wait_for_button();
+
+   //cosine
+   /*
+   Function s4 {dcos,r_min,r_max,orig,400,30,30};
+   s4.set_color(Color::blue);
+
+   Function s5 {sloping_cos, r_min,r_max,orig,400,30,30};
+   s5.set_color(Color::green);
+
+   x.label.move(-160,0);
+   x.notches.set_color(Color::dark_red);
+
+   win.attach(s4);
+   win.attach(s5);
+   win.wait_for_button();
+    */
+
+   //Function s5 {[](double x) −> double { return cos(x)+slope(x); },r_min,r_max,orig,400,30,30};
+   // this contains a lamda function.
+   //[] lambda introducer (used to give access to local variables)
+   //(argument list) -> return type (suffix notation)
+   //{function body}
+
+   for (int n = 0; n<50; ++n) {
+       ostringstream ss;
+       ss << "exp approximation; n==" << n ;
+       win.set_label(ss.str());
+
+       // get next approximation:
+       Function e {[n](double x) { return exp_n(x,n); },r_min,r_max,orig,200,x_scale,y_scale};
+
+       win.attach(e);
+       win.wait_for_button();
+       win.detach(e);
+       //this starts breaking for n == 15 and completely breaks after n== 19 crashes after n== 50
+   }
+
+
 }
