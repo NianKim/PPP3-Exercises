f617f370d4fc6a56020f3ac1d5138caee6b59ca2
Nian Kim <niankim@gmail.com>
2026-03-17 18:50:17 +0100

    completed up to subtask 9

--- Chapter15/drill.cpp
+++ Chapter15/drill.cpp
@@ -4,10 +4,21 @@
 //[1] Allocate an array of ten ints on the free store using new.
 //[2] Print the values of the ten ints to cout.
 //[3] Deallocate the array (using delete[]).
-int main(){
-    int* int_array = new int[10];
-    for ( int i = 0; i < 10; ++i){
-        std::cout << "Entry " << i << ": " << int_array[i] << '\n';
+
+void print_array(std::ostream& os, int* a, int n){
+    //print out values of a assuming n entries
+    for(int i = 0; i < n; ++i){
+        os << a[i] << " ";
     }
+    os << std::endl;
+}
+
+int main(){
+    //[7] Allocate an array of 20 ints on the free store; initialize it with the values 100, 101, 102, etc.; and print out its values.
+    int* int_array = new int[20];
+    for ( int i = 0; i < 20; ++i){
+        int_array[i] = 100 + i;
+    }
+    print_array(std::cout, int_array, 20);
     delete[] int_array;
 }
