9b80efb010e38359da006964381155de4928f833
Nian Kim <niankim@gmail.com>
2026-04-15 19:03:13 +0200

    created first try_this01 using vector03.cpp. The new [] operator cannot write rvalues, only read lvalues

---
+++ Chapter17/vector03.cpp
@@ -0,0 +1,32 @@
+class Vector {                  //very simplyfied vector of doubles
+public:
+    Vector(int s);              //constructor
+    Vector() { delete[] elem; } //destructor, return elements to the free store
+    int size() const { return sz; }
+    double get(int n) const {return elem[n];}
+    void set(int n, double v) {elem[n]=v;}
+    double operator[](int n) { return elem[n]; } // requires an lvalue, will not be able to write, only read.
+private:
+    int sz;                     //size
+    double* elem;               //pointer to elements
+};
+Vector::Vector(int s)           //constructor
+    :sz{s},
+    elem{new double[s]}         //initialize to elements on the free store
+{
+    for (int i=0; i<s; ++i)     //initialize elements
+        elem[i] = 0;
+}
+
+#include<iostream>
+
+int main(){
+    Vector v(5);
+    Vector v(10);
+    double x = v[2];                // fine
+    v[3] = x; 
+for (int i=0; i<v.size(); ++i) {
+        v.set(i,1.1*i);
+        std::cout << "v[" << i << "]==" << v.get(i) << '\n';
+}
+}
