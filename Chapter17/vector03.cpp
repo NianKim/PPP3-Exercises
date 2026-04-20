66f7cf4673bef568801abab4a565c19edc779b30
Nian Kim <niankim@gmail.com>
2026-04-20 20:33:27 +0200

    added a primitive copy assignment with the operator '='.

--- Chapter17/vector03.cpp
+++ Chapter17/vector03.cpp
@@ -23,6 +23,7 @@ public:
     const double& operator[] (int n) const { return elem[n]; }  //return a const& for a const
 
     Vector (const Vector&);          //copy constructor: define copy
+    Vector& operator=(const Vector&);
 private:
     int sz;                     //size
     double* elem;               //pointer to elements
@@ -32,6 +33,16 @@ private:
         :sz{arg.sz}, elem{new double[arg.sz]}
     {
         std::copy(arg.elem,arg.elem+sz,elem);  // copy elements [0:sz) from elem.arg into elem
+    }
+
+    Vector& Vector::operator=(const Vector& a)  //make assigned Vector a copy of a
+    {
+        double* p = new double[a.sz];           //allocate new space
+        std::copy(a.elem,a.elem+a.sz,p);        //copy element [0:sz) from a.elem into p
+        delete[] elem;                          //deallocate old space
+        elem = p;                               //now we can reset elem
+        sz = a.sz;
+        return *this;                           //return a self-reference
     }
 
 #include<iostream>
@@ -49,6 +60,9 @@ int main(){
     }
 
     v1[1] = 99;                             
-    v2[0] = 88;                                
+    v2[0] = 88;                               
     std::cout << "\nv2[0]: " << v2[0] << ", v1[1]: " << v1[1] << "\n";
+    for(int i = 0; i < v3.size(); ++i){
+        std::cout << "v3[" << i << "]: " << v3[i] << '\n';
+    }
 }
