e2fcdcc70c81ee490a7d627ae0615cf3cb47555d
Nian Kim <niankim@gmail.com>
2026-04-22 18:35:04 +0200

    implemented first half of try_this02

---
+++ Chapter17/example_print.cpp
@@ -0,0 +1,56 @@
+//a good way to get a feel for constructor, assignment and destructor is to print
+/*
+Try This
+Try executing that. Then remove the move operations and run it again. 
+The compilers can be very clever at avoiding unnecessary copies. 
+We really mean it: do run this example and try to explain the result. 
+If you do, you’ll understand most of what there is to know about 
+construction and destruction of objects.
+*/
+
+#include <iostream>
+#include <vector>
+
+struct X {
+    //a simple test class
+
+    int val;
+    void out(const std::string& s, int nv) { std::cout << this << "->" << s << ": " << val << " (" << nv << ")\n"; }
+
+       X(){ out("X()",0); val=0; }                                                           // default constructor
+       X(int x) { out( "X(int)",x); val=x; }
+       X(const X& x){out("X(X&) ",x.val); val=x.val; }                          // copy constructor
+       X(X&& x){ out("X(X&&) ",x.val); val=x.val; x.val=0; }                // move constructor
+       X& operator=(const X& x) { out("X copy assignment",x.val); val=x.val; return *this; }
+       X& operator=(X&& x) { out("X move assignment",x.val); val=x.val; x.val=0; return *this; }
+       ~X() { out("~X()",0); } 
+};
+
+
+X glob {2};                                       // a global variable
+
+X copy(X a) { std::cout << "copy()\n"; return a; }
+X copy2(X a) { std::cout << "copy2()\n"; X aa = a; return aa; }
+X& ref_to(X & a) { std::cout << "ref_to()\n"; return a; }
+X* make(int i) { std::cout << "make()\n"; X a(i); return new X(a); }
+
+struct XX { X a; X b; };              // members
+
+int main()
+{
+        X loc {4};                         // local variable
+        X loc2 {loc};                    // copy construction
+        loc = X{5};                       // copy assignment
+        loc2 = copy(loc);             // call by value and return
+        loc2 = copy2(loc);
+        X loc3 {6};
+        X& r = ref_to(loc);      // call by reference and return
+        delete make(7);
+        delete make(8);
+        std::vector<X> v(4);               // default values
+        XX loc4;
+        X* p = new X{9};             // an X on the free store
+        delete p;
+        X* pp = new X[5];           // an array of Xs on the free store
+        delete[] pp;
+}
