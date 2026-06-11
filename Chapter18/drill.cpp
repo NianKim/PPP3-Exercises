a6c3464a9355a69aed78c9d0f3ed8c30c8206881
Nian Kim <niankim@gmail.com>
2026-06-11 17:07:39 +0200

    completed step 3

--- Chapter18/drill.cpp
+++ Chapter18/drill.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
-
+#include<string>
+#include<vector>
 
 template<typename T> 
 struct S {
@@ -10,10 +11,17 @@ struct S {
 
 int main() {
 
+    using SString = S<std::string>;
+    using SVector = S<std::vector<int>>;
+
     S<int> a = 67;
     S<char> b = 'v';
     S<double> c = 3.14;
 
+    SString d{"Templates!"};
+    SVector e{ { 1,2,3,4 }};
+
+    //std::cout << a << b << c << d << e << '\n';
 
     std::cout << "Successfully compiled!" << '\n';
     return 0;
