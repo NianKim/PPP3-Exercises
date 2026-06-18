04952bfa5e646475de688c8e2e90681233359b61
Nian Kim <niankim@gmail.com>
2026-06-18 16:11:25 +0200

    Implemented try_this03.cpp using iterators

--- Chapter19/try_this03.cpp
+++ Chapter19/try_this03.cpp
@@ -1,12 +1,35 @@
-#include <iostream>
-#include <vector>
-
 
 //We again left a serious error in that program.
 // Find it, fix it, and suggest a general remedy for that kind of problem.
 
-double* get_from_jack(int* count);           // Jack fills an array and puts the number of elements in *count
-std::vector<double> get_from_jill();                // Jill fills the vector
+#include <vector>
+#include <iostream>
+
+double* get_from_jack(int* count){
+    *count = 3;
+    double* data = new double[3] { -12312.3, 4.32, -432.32 };
+    return data;
+}
+//Jack fills an array and puts the number of elements in *count
+
+std::vector<double> get_from_jill(){
+    return {-32.33, -234.33, 654.3};
+}     //Jill fills a vector
+
+
+template<typename Iter>
+Iter high(Iter first, Iter last) {
+    if (first == last) return last; 
+
+    Iter high_ptr = first;
+    ++first; // Start comparing from the second element
+    for (Iter p = first; p != last; ++p) {
+        if (*high_ptr < *p) {
+            high_ptr = p;
+        }
+    }
+    return high_ptr;
+}
 
 void fct()
 {
@@ -14,9 +37,25 @@ void fct()
         double* jack_data = get_from_jack(&jack_count);
         std::vector<double> jill_data = get_from_jill();
 
-        double* jack_high = high(jack_data,jack_data+jack_count);
-        double* jill_high = high(jill_data.begin(),jill_data.end());
+        auto jack_high = high(jack_data,jack_data+jack_count);
+        auto jill_high = high(jill_data.begin(),jill_data.end());
 
-        std::cout << "Jill's high " << *jill_high << "; Jack's high " << *jack_high;
+        // Protect against empty containers by checking against the 'end'
+        if (jack_high != (jack_data + jack_count)) {
+            std::cout << "Jack's high " << *jack_high << "\n";
+        } else {
+            std::cout << "Jack has no data.\n";
+        }
+
+        if (jill_high != jill_data.end()) {
+            std::cout << "Jill's high " << *jill_high << "\n";
+        } else {
+            std::cout << "Jill has no data.\n";
+        }
         delete[] jack_data;
+}
+
+int main(){
+    fct();
+    return 0;
 }
