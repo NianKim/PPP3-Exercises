8a00b1ff87f657841a3e826da9b30d65d0a51929
Nian Kim <niankim@gmail.com>
2026-06-19 19:53:49 +0200

    expanded upon link.cpp

--- Chapter19/link.cpp
+++ Chapter19/link.cpp
@@ -1,3 +1,10 @@
+/*
+The purpose of our List is to convey an understanding of 
+what linked lists are, how a list might be implemented, 
+and how to use the key features. For more information 
+see an expert-level C++ book.
+*/
+
 template<typename T>
 struct Link {
     Link* prev;
@@ -5,7 +12,25 @@ struct Link {
     T val;
 };
 
-template<typename T> struct List {
+template<typename T> class List {
     Link<T>* first;
     Link<T>* last;      //one beyond the last link
+
+    public:
+        class iterator; //member type
+        
+        iterator begin();
+        iterator end(); //one beyond the last link
+
+        iterator insert(iterator p, const T& v);    //insert after p
+        iterator erase(iterator p);                 //erase p
+
+        void push_back(const T& v);                 //insert v at end
+        void push_front(const T& v);                //insert v at front
+        void pop_front();                           //remove the first element
+        void pop_back();                            //remove the last element
+
+
+        T& front();     //the first element
+        T& back();      //the last element
 };
