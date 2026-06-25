8d8fc47533a015550cda4ce915e223a10b4d3830
Nian Kim <niankim@gmail.com>
2026-06-25 13:02:53 +0200

    first 3 steps

--- Chapter19/drill.cpp
+++ Chapter19/drill.cpp
@@ -1,6 +1,10 @@
 // [1] Define an array of ints with the ten elements { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }.
 #include<iostream>
+#include<vector>
+#include<list>
 
 int main(){
     int my_array[10] = {0,1,2,3,4,5,6,7,8,9}; 
+    std::vector<int> my_vector = {0,1,2,3,4,5,6,7,8,9};
+    std::list<int> my_list = {0,1,2,3,4,5,6,7,8,9};
 }
