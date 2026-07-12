3d41ccff4cee7c566a2c4b110655d6745f8add72
Nian Kim <niankim@gmail.com>
2026-07-12 22:27:50 +0200

    finished drill

--- Chapter21/drill.cpp
+++ Chapter21/drill.cpp
@@ -5,6 +5,8 @@
 #include <vector>
 #include <fstream>
 #include <iterator>
+#include <numeric>
+#include <algorithm>
 
 using namespace std;
 
@@ -26,9 +28,43 @@ int main() {
 
     vector<double> vd(ii, eos);
 
-    // [2] Output vd to cout
+    //[3] Make a vector vi of type vector<int> with the same number 
+    //of elements as vd; copy the elements from vd into vi.
+    vector<int> vi(vd.begin(), vd.end());
+
+
+    // [4] Output (vd[i],vi[i]) to cout
     cout << "\n--- Vector Contents ---\n";
-    for (double d : vd) {
-        cout << d << '\n';
+    for (int i = 0; i < 16; ++i) {
+        cout << '(' << vd[i] << ", " << vi[i] << ')' << '\n';
     }
+
+    //[5] Output the sum of the elements of vd.
+    double vd_total = std::accumulate(vd.begin(), vd.end(), 0.0);
+        std::cout << "Total sum: " << vd_total << '\n';
+
+    //[6] Output the difference between the sum of the elements of vd and the sum of the elements of vi.
+    int vi_total = std::accumulate(vi.begin(), vi.end(), 0); 
+        std::cout << "Difference in sums: " << vi_total - vd_total << '\n';
+
+    //[7] There is a standard-library algorithm called reverse that takes a sequence (pair of iterators) as arguments; reverse vd, and output vd to cout.
+    reverse(vd.begin(), vd.end());
+    cout << "\n--- Reversed vd ---\n";
+    for (double d : vd) cout << d << ' ';
+    cout << '\n';
+
+    // [8] Compute the mean value of the elements in vd; output it.
+    double mean = vd_total / vd.size();
+    cout << "\nMean value of vd: " << mean << '\n';
+
+    // [9] Make a new vector<double> called vd2 and copy all elements of vd 
+    // with values lower than (less than) the mean into vd2.
+    vector<double> vd2;
+    std::copy_if(vd.begin(), vd.end(), std::back_inserter(vd2), [mean](double x) {
+        return x < mean;
+    });
+
+    cout << "\n--- Elements in vd2 (less than mean) ---\n";
+    for (double d : vd2) cout << d << ' ';
+    cout << '\n';
 }
